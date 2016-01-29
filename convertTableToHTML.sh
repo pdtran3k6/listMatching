48d6b16e607f047f56e2f17f5e0f743542452ce1
Phat Tran <phatdtran3k6@gmail.com>
2016-01-29 15:32:53 -0500

    Update convertTableToHTML.sh
    Add another column at the very end to show the matched hosts across sources and show the total amount + percentage presence. 
    Also remove unnecessary linkage from title and attributes headers (Hostname, Num_Host_Matched, Percentage_Matched)

--- convertTableToHTML.sh
+++ convertTableToHTML.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/ksh
-
+#!/usr/bin/ksh 	
+MASTER=Master
 MASTER_TABLE_DIR=/u1/tranp 
+
 
 cd $MASTER_TABLE_DIR
 rm table.html
 echo "<html>\n" >> table.html
 	
-#Set the spacing between columns
+# Set the spacing between columns
 echo "<head>
 <style type="text/css">
 td
@@ -17,24 +18,35 @@ td
 </head>\n" >> table.html
 echo "<body align='center'>\n" >> table.html
 date >> table.html
+
+# Title of the table
 echo "\n<h3>List of Hosts from Different Sources</h3>\n" >> table.html
 echo "<table align='center'>" >> table.html
 
 
-#Add each columns and each rows into table format
-while read col1 col2 col3 col4 col5
+# Add each columns and each rows into table format
+while read col1 col2 col3 col4 col5 col6
 do
 	echo "<tr>" >> table.html
-	echo "<td><a href='/Apache/inv/$col1.txt'>$col1</td>" >> table.html
+	# Link to host info file only if the it's the host is in the Masterlist
+	grep "$col1" $MASTER > /dev/null
+	if [ $? -eq 0 ]
+	then
+		echo "<td><a href='/inv/$col1.txt'>$col1</td>" >> table.html
+	else
+		echo "<td>$col1</td>" >> table.html
+	fi
+	
 	echo "<td>$col2</td>" >> table.html
 	echo "<td>$col3</td>" >> table.html
 	echo "<td>$col4</td>" >> table.html
 	echo "<td>$col5</td>" >> table.html
+	echo "<td>$col6</td>" >> table.html
 	echo "</tr>" >> table.html
 	echo "\n" >> table.html
 done < MasterTable	
 	
-#Close table and html
+# Close table and html
 print "\n</table>\n</body>\n</html>" >> table.html
 
 cat table.html
